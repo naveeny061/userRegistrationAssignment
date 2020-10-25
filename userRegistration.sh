a3cb17f727d308fc528adef62066a6a8871bad0d
naveeny061 <naveeny061@gmail.com>
2020-10-25 18:56:43 +0530

    add: Last Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -x
 echo "welcome to user  registration program"
 
-read -p "enter name " name
+read -p "enter first name " firstName
 pattern1="^[A-Z][a-z]{2}[a-z]*$"
-if [[ $name =~ $pattern1 ]]
+if [[ $firstName =~ $pattern1 ]]
+then
+echo "Name is valid"
+else
+echo "Name is invalid"
+fi
+
+read -p "enter last name" lastName
+pattern2="^[A-Z][a-z]{2}[a-z]*$"
+if [[ $lastName =~ $pattern2 ]]
 then
 echo "Name is valid"
 else
