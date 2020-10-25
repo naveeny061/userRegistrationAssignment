6568911418859eb54a9ec43ca54218fe1e6b25a7
naveeny061 <naveeny061@gmail.com>
2020-10-25 18:50:15 +0530

    add: First Name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -x
 echo "welcome to user  registration program"
+
+read -p "enter name " name
+pattern1="^[A-Z][a-z]{2}[a-z]*$"
+if [[ $name =~ $pattern1 ]]
+then
+echo "Name is valid"
+else
+echo "Name is invalid"
+fi
+
+
