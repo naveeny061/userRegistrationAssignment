bb4dcc7145c4cc956032b43cd9ee79eaa61d8d1c
naveeny061 <naveeny061@gmail.com>
2020-10-25 19:30:28 +0530

    add: Email ID

--- userRegistration.sh
+++ userRegistration.sh
@@ -10,7 +10,7 @@ else
 echo "Name is invalid"
 fi
 
-read -p "enter last name" lastName
+read -p "enter last name " lastName
 pattern2="^[A-Z][a-z]{2}[a-z]*$"
 if [[ $lastName =~ $pattern2 ]]
 then
@@ -19,4 +19,12 @@ else
 echo "Name is invalid"
 fi
 
+read -p "enter email " email
+pattern3="^[a-zA-Z][a-zA-Z0-9]*([\.][a-zA-Z0-9]+)*(@[a-zA-Z0-9]+[\.][a-zA-Z0-9]{2,})([/.][a-zA-Z0-9]{2})?$"
+if [[ $email =~ $pattern3 ]]
+then
+echo "email is valid"
+else
+echo "email is invalid"
+fi
 
